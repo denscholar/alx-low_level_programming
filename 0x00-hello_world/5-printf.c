83a084f067ee0cb4dcaf1d0b5660fc3b67f75cd5
Dennis <d.akagha20@gmail.com>
2022-09-11 15:49:11 -0700

    Add a C program that prints exactly with proper grammer...

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print - "with proper grammer, but the output is a place of art,"
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
